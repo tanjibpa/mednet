a66408a25af6a5e4f4ffc69460f739957c3131a9
Ikramul Alam <ikram.tanjib@gmail.com>
2021-04-09 13:14:50 -0400

    inventory crud added

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from inventory.views import ProductsListView, ProductCreateView
+from inventory.views import ProductsListView, ProductCreateView, ProductDetailView, ProductUpdateView
 
 app_name = 'inventory'
 urlpatterns = [
     path('list', ProductsListView.as_view(), name='product_list_view'),
-    path('create', ProductCreateView.as_view(), name='product_create_view')
+    path('create', ProductCreateView.as_view(), name='product_create_view'),
+    path('detail/<int:pk>', ProductDetailView.as_view(), name='product_detail_view'),
+    path('update/<int:pk>', ProductUpdateView.as_view(), name='product_update_view'),
 ]
--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, FormView, UpdateView, DetailView
 
 from inventory.forms import ProductForm
 from inventory.models import Product
@@ -19,9 +19,28 @@ class ProductCreateView(CreateView):
     model = Product
     template_name = 'inventory/create.html'
     form_class = ProductForm
-    success_url = reverse_lazy('inventory:product_list_view')
+
+    def get_success_url(self):
+        return reverse_lazy('inventory:product_detail_view', kwargs={'pk': self.object.id})
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.producer = self.request.organization
         return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'inventory/update.html'
+    form_class = ProductForm
+    queryset = Product.objects.all()
+
+    def get_success_url(self):
+        return reverse_lazy('inventory:product_detail_view', kwargs={'pk': self.object.id})
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'inventory/detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
